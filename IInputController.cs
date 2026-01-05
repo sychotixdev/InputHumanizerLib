ceeafab60cbf20c3ff6163ffcf7baee2e5d14346
Sychotix <8293864+sychotixdev@users.noreply.github.com>
2026-01-05 10:56:36 +0100

    Added release control command.

--- IInputController.cs
+++ IInputController.cs
@@ -19,8 +19,8 @@ namespace InputHumanizer.Input
         SyncTask<bool> MoveMouse(Vector2 coordinate, CancellationToken cancellationToken = default);
         SyncTask<bool> MoveMouse(Vector2 coordinate, int maxInterpolationDistance, int minInterpolationDelay, int maxInterpolationDelay, CancellationToken cancellationToken = default);
         SyncTask<bool> ClearMousePosition(CancellationToken cancellationToken = default);
-
         SyncTask<Vector2?> GetCursorPos(CancellationToken cancellationToken = default);
+        SyncTask<bool> ReleaseControl(CancellationToken cancellationToken = default);
 
         int GenerateDelay();
     }
