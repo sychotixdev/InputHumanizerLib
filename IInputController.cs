1379f1f4edaa118c4e9a384ac9693feaf567bcdf
Sychotix <8293864+sychotixdev@users.noreply.github.com>
2026-01-24 18:11:20 +0100

    Switch on compiler directives

--- IInputController.cs
+++ IInputController.cs
@@ -1,26 +1,30 @@
-﻿using System;
-using System.Numerics;
-using System.Threading;
-using System.Windows.Forms;
-using ExileCore2.Shared;
+﻿using System.Numerics;
+
+#if POE1
+using Shared = ExileCore.Shared;
+
+#else
+using Shared = ExileCore2.Shared;
+
+#endif
 
 namespace InputHumanizer.Input
 {
     public interface IInputController : IDisposable
     {
-        SyncTask<bool> KeyDown(Keys key, CancellationToken cancellationToken = default);
-        SyncTask<bool> KeyUp(Keys key, bool releaseImmediately = false, CancellationToken cancellationToken = default);
-        SyncTask<bool> Click(CancellationToken cancellationToken = default);
-        SyncTask<bool> Click(MouseButtons button, CancellationToken cancellationToken = default);
-        SyncTask<bool> Click(MouseButtons button, Vector2? coordinate, CancellationToken cancellationToken = default);
-        SyncTask<bool> ClickWithModifiers(MouseButtons button, Vector2? coordinate, MouseModifiers modifiers, CancellationToken cancellationToken = default);
-        SyncTask<bool> VerticalScroll(bool forward, int numClicks, CancellationToken cancellationToken = default);
-        SyncTask<bool> VerticalScroll(bool forward, int numClicks, Vector2? coordinate, CancellationToken cancellationToken = default);
-        SyncTask<bool> MoveMouse(Vector2 coordinate, CancellationToken cancellationToken = default);
-        SyncTask<bool> MoveMouse(Vector2 coordinate, int maxInterpolationDistance, int minInterpolationDelay, int maxInterpolationDelay, CancellationToken cancellationToken = default);
-        SyncTask<bool> ClearMousePosition(CancellationToken cancellationToken = default);
-        SyncTask<Vector2?> GetCursorPos(CancellationToken cancellationToken = default);
-        SyncTask<bool> ReleaseControl(CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> KeyDown(Keys key, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> KeyUp(Keys key, bool releaseImmediately = false, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> Click(CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> Click(MouseButtons button, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> Click(MouseButtons button, Vector2? coordinate, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> ClickWithModifiers(MouseButtons button, Vector2? coordinate, MouseModifiers modifiers, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> VerticalScroll(bool forward, int numClicks, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> VerticalScroll(bool forward, int numClicks, Vector2? coordinate, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> MoveMouse(Vector2 coordinate, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> MoveMouse(Vector2 coordinate, int maxInterpolationDistance, int minInterpolationDelay, int maxInterpolationDelay, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> ClearMousePosition(CancellationToken cancellationToken = default);
+        Shared.SyncTask<Vector2?> GetCursorPos(CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> ReleaseControl(CancellationToken cancellationToken = default);
 
         int GenerateDelay();
     }
