5c729ca63f97ba95defa33e7c0ad37d0aa465585
Sychotix <8293864+sychotixdev@users.noreply.github.com>
2026-02-05 15:41:39 +0100

    Add the ability to override delays for your pulled instance

--- IInputController.cs
+++ IInputController.cs
@@ -22,9 +22,11 @@ namespace InputHumanizer.Input
         Shared.SyncTask<bool> VerticalScroll(bool forward, int numClicks, Vector2? coordinate, CancellationToken cancellationToken = default);
         Shared.SyncTask<bool> MoveMouse(Vector2 coordinate, CancellationToken cancellationToken = default);
         Shared.SyncTask<bool> MoveMouse(Vector2 coordinate, int maxInterpolationDistance, int minInterpolationDelay, int maxInterpolationDelay, CancellationToken cancellationToken = default);
+        Shared.SyncTask<bool> MoveMouseWindMouseImpl(Vector2 coordinate, double gravityStrength, double windStrength, int minInterpolationDelay, int maxInterpolationDelay, double stepSize, double targetArea, CancellationToken cancellationToken = default);
         Shared.SyncTask<bool> ClearMousePosition(CancellationToken cancellationToken = default);
         Shared.SyncTask<Vector2?> GetCursorPos(CancellationToken cancellationToken = default);
         Shared.SyncTask<bool> ReleaseControl(CancellationToken cancellationToken = default);
+        void SetDelayOverrides(int minDelay = -1, int maxDelay = -1, int mean = -1, int standardDeviation = -1);
 
         int GenerateDelay();
     }
